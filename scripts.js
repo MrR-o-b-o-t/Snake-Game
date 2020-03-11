ff3d36c8719bfc62d2d2ee62dfe35d7dce6a5cc5
Sam Kautz <samkautz@yahoo.com>
2020-03-10 21:11:09 -0500

    Add snake movement

--- scripts.js
+++ scripts.js
@@ -1,29 +1,34 @@
 
-let canvas;
-let canvasContext;
-let snakeX = 278;
-let snakeY = 200;
+let canvas = document.getElementById('gameCanvas');
+let ctx = canvas.getContext('2d')
+let direction = false;
+let snakeX = canvas.width / 2;
+let snakeY = canvas.height - canvas.height / 2;
+let snakeSpeedX = 2;
+let snakeSpeedY;
 
 window.onload = function () {
+    ctx.beginPath()
     window.addEventListener('keydown', moveSnake, false);
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d')
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-    drawSnake();
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     moveSnake();
 }
 
 function drawSnake() {
-    canvasContext.fillStyle = 'green'
-    canvasContext.fillRect(200, 200, 25, 25);
-    canvasContext.fillStyle = 'green'
-    canvasContext.fillRect(226, 200, 25, 25);
-    canvasContext.fillStyle = 'green'
-    canvasContext.fillRect(252, 200, 25, 25);
-    canvasContext.fillStyle = 'green'
-    canvasContext.fillRect(snakeX, snakeY, 25, 25);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = 'green'
+    ctx.fillRect(snakeX, snakeY, 25, 25);
 }
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawSnake();
+    snakeX += snakeSpeedX
+}
+
+setInterval(draw, 70);
+
 
 function moveSnake(e) {
     switch (e.keyCode) {
