6740373cb4cea982777fd06d011c5a7f757e8a3d
Sam Kautz <samkautz@yahoo.com>
2020-03-20 21:21:08 -0500

    Improve snake movement. Added preventDefault() for movement keys.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 
 let canvas = document.getElementById('gameCanvas')
 let ctx = canvas.getContext('2d')
-let snakeDirection
 document.addEventListener("keydown", moveSnakeKeys, false)
 let appleX
 let appleY
 let score = 0
+let snakeDirection;
 
 createApple()
 
 class Snake {
     constructor() {
         this.body = [
-            { x: 50, y: canvas.height / 2 },
-            { x: 34, y: canvas.height / 2 },
-            { x: 18, y: canvas.height / 2 }
+            { x: 50, y: 200 },
+            { x: 36, y: 200 },
+            { x: 21, y: 200 }
         ]
         this.snakeSpeed = 15
         this.color = 'green'
@@ -29,30 +29,22 @@ class Snake {
         ctx.fill()
     }
 
-    updateSnake() {
-        if (snakeDirection === "right") {
-            for (let i = 0; i < this.body.length; i++) {
-                this.body[i].x += this.snakeSpeed
-            }
-        } else if (snakeDirection === 'down') {
-            for (let i = 0; i < this.body.length; i++) {
-                this.body[i].y += this.snakeSpeed
-            }
-        } else if (snakeDirection === 'left') {
-            for (let i = 0; i < this.body.length; i++) {
-                this.body[i].x -= this.snakeSpeed
-            }
-        } else if (snakeDirection === 'up') {
-            for (let i = 0; i < this.body.length; i++) {
-                this.body[i].y -= this.snakeSpeed
-            }
-        }
-        this.drawSnake()
+    // check direction of snake (if moving)
+    // remove snake 'tail'
+    // check empty space at head of snake using snakeDirection 
+    // replace that empty space with snake 'tail'
 
-        if (this.body[0].x > canvas.width - this.snakeSize || this.body[0].x < 0) {
-            alert('Game Over!')
-        } else if (this.body[0].y > canvas.height - this.snakeSize || this.body[0].y < 0) {
-            alert('Game Over!')
+    updateSnake() {
+        const dir = {
+            up: { x: 0, y: -this.snakeSize },
+            down: { x: 0, y: this.snakeSize },
+            left: { x: -this.snakeSize, y: 0 },
+            right: { x: this.snakeSize, y: 0 },
+        }[snakeDirection] || { x: 0, y: 0 };
+        if (snakeDirection != undefined) {
+            this.body.unshift(this.body.pop());
+            this.body[0].x = this.body[1].x + dir.x;
+            this.body[0].y = this.body[1].y + dir.y;
         }
     }
 }
@@ -66,23 +58,22 @@ function animate() {
     ctx.fillStyle = 'red';
     ctx.fillRect(appleX, appleY, 15, 15)
     ctx.fill()
+    snake.drawSnake();
     updateScore()
+    gameOver()
 }
 
 setInterval(animate, 200)
 
 function moveSnakeKeys(e) {
+    e.preventDefault()
     if (e.code === "ArrowRight" && snakeDirection != "left") {
-        console.log("snake arrow right")
         snakeDirection = "right";
     } else if (e.code === "ArrowLeft" && snakeDirection != "right") {
         snakeDirection = "left";
-        console.log("snake arrow left")
     } else if (e.code === "ArrowUp" && snakeDirection != "down") {
-        console.log("snake arrow up")
         snakeDirection = "up";
     } else if (e.code === "ArrowDown" && snakeDirection != "up") {
-        console.log("snake arrow down")
         snakeDirection = "down";
     }
 }
@@ -102,13 +93,21 @@ function updateScore() {
         createApple()
         score = score + 1
         document.getElementById('score').innerText = score
-        let xLength = snake.body[snake.body.length - 1].x - 16
-        let yLength = snake.body[snake.body.length - 1].y
-        snake.body.push({ x: xLength, y: yLength })
+        growSnake()
     }
 }
 
-// if the snake eats the apple
-// check direction of snake
-// add new x and y values to snakeBody array
-// add new snake body block 'behind' snake head
+function growSnake() {
+    let xLength = snake.body[snake.body.length - 1].x - 16
+    let yLength = snake.body[snake.body.length - 1].y
+    snake.body.push({ x: xLength, y: yLength })
+}
+
+function gameOver() {
+    if (snake.body[0].x > canvas.width - snake.snakeSpeed || snake.body[0].x < 0) {
+        alert('Game Over!')
+    } else if (snake.body[0].y > canvas.height - snake.snakeSpeed || snake.body[0].y < 0) {
+        alert('Game Over!')
+    }
+}
+
