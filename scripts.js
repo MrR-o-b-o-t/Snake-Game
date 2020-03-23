0750e8b5d44b9f4a3ad7bcd5d834d56471e73432
Sam Kautz <samkautz@yahoo.com>
2020-03-22 22:12:54 -0500

    Add snake graphics, game sounds, and end game display screen.

--- scripts.js
+++ scripts.js
@@ -6,33 +6,45 @@ let appleX
 let appleY
 let score = 0
 let snakeDirection;
+let grid = 15
+let gameIsOver = false;
+
+function handleMouseClick() {
+    if (gameIsOver) {
+        // ctx.fillStyle = "White"
+        // ctx.font = "15px Arial"
+        // ctx.fillText(`Game Over! You scored ${score} points! Click to continue.`, 100, 100)
+        gameIsOver = false
+        score = 0
+        document.getElementById('score').innerText = 0
+    }
+}
 
 createApple()
+canvas.addEventListener("mousedown", handleMouseClick)
 
 class Snake {
     constructor() {
         this.body = [
-            { x: 50, y: 200 },
-            { x: 36, y: 200 },
-            { x: 21, y: 200 }
+            { x: 45, y: 195 },
+            { x: 30, y: 195 },
+            { x: 15, y: 195 }
         ]
-        this.snakeSpeed = 15
+        this.snakeSpeed = grid
         this.color = 'green'
-        this.snakeSize = 15
+        this.snakeSize = grid
     }
 
     drawSnake() {
         ctx.fillStyle = this.color;
         for (let i = 0; i < this.body.length; i++) {
             ctx.fillRect(this.body[i].x, this.body[i].y, this.snakeSize, this.snakeSize)
+            ctx.strokeStyle = "white"
+            ctx.strokeRect(this.body[i].x, this.body[i].y, this.snakeSize, this.snakeSize)
         }
         ctx.fill()
     }
 
-    // check direction of snake (if moving)
-    // remove snake 'tail'
-    // check empty space at head of snake using snakeDirection 
-    // replace that empty space with snake 'tail'
 
     updateSnake() {
         const dir = {
@@ -54,16 +66,24 @@ const snake = new Snake()
 function animate() {
     ctx.fillStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+    resetGame()
+    if (gameIsOver) {
+        return
+    } else {
+        handleMouseClick()
+    }
     snake.updateSnake()
     ctx.fillStyle = 'red';
-    ctx.fillRect(appleX, appleY, 15, 15)
+    ctx.fillRect(appleX, appleY, grid, grid)
     ctx.fill()
     snake.drawSnake();
     updateScore()
+    snakeWallCollision()
+    snakeCollision()
     gameOver()
 }
 
-setInterval(animate, 200)
+setInterval(animate, 100)
 
 function moveSnakeKeys(e) {
     e.preventDefault()
@@ -79,17 +99,19 @@ function moveSnakeKeys(e) {
 }
 
 function createApple() {
-    appleX = Math.floor((Math.random() * (canvas.width - 15)))
-    appleY = Math.floor((Math.random() * (canvas.height - 15)))
+    appleX = Math.floor((Math.random() * (canvas.width / grid))) * grid
+    appleY = Math.floor((Math.random() * (canvas.height / grid))) * grid
 }
 
 function updateScore() {
-    if (snake.body[0].x < appleX + 15 &&
-        snake.body[0].x + 15 > appleX &&
-        snake.body[0].y < appleY + 15 &&
-        snake.body[0].y + 15 > appleY
+    if (snake.body[0].x < appleX + grid &&
+        snake.body[0].x + grid > appleX &&
+        snake.body[0].y < appleY + grid &&
+        snake.body[0].y + grid > appleY
     ) {
-        ctx.clearRect(appleX, appleY, 15, 15)
+        let EatSound = new Audio("sounds/EatSound.ogg")
+        EatSound.play()
+        ctx.clearRect(appleX, appleY, grid, grid)
         createApple()
         score = score + 1
         document.getElementById('score').innerText = score
@@ -98,16 +120,46 @@ function updateScore() {
 }
 
 function growSnake() {
-    let xLength = snake.body[snake.body.length - 1].x - 16
+    let xLength = snake.body[snake.body.length - 1].x - grid + 1
     let yLength = snake.body[snake.body.length - 1].y
     snake.body.push({ x: xLength, y: yLength })
 }
 
 function gameOver() {
-    if (snake.body[0].x > canvas.width - snake.snakeSpeed || snake.body[0].x < 0) {
-        alert('Game Over!')
-    } else if (snake.body[0].y > canvas.height - snake.snakeSpeed || snake.body[0].y < 0) {
-        alert('Game Over!')
+    if (gameIsOver === true) {
+        snake.body = [
+            { x: 45, y: 195 },
+            { x: 30, y: 195 },
+            { x: 15, y: 195 }
+        ]
+        snakeDirection = undefined
     }
 }
 
+function snakeWallCollision() {
+    if (snake.body[0].x > canvas.width - snake.snakeSpeed + 10 || snake.body[0].x < 0 - 1) {
+        gameIsOver = true
+    } else if (snake.body[0].y > canvas.height - snake.snakeSpeed + 10 || snake.body[0].y < 0 - 10) {
+        gameIsOver = true
+    }
+}
+
+function snakeCollision() {
+    for (let i = 1; i < snake.body.length; i++) {
+        let x = snake.body[0].x
+        let y = snake.body[0].y
+        let snakeBodyY = snake.body[i].y
+        let snakeBodyX = snake.body[i].x
+        if (x == snakeBodyX && y == snakeBodyY) {
+            gameIsOver = true
+        }
+    }
+}
+
+function resetGame(e) {
+    if (gameIsOver) {
+        ctx.fillStyle = "White"
+        ctx.font = "20px Arial"
+        ctx.fillText(`Game Over! You score was ${score}! Click anywhere to continue`, 25, 100)
+    }
+}
